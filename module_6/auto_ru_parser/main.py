17378b308586cf2186d9dd38f94bb3996773b606
Segodnya <75853148+Segodnya@users.noreply.github.com>
2021-08-21 09:39:55 +0300

    fix output data structure

--- module_6/auto_ru_parser/main.py
+++ module_6/auto_ru_parser/main.py
@@ -10,13 +10,13 @@ from google.colab import drive
 drive.mount('/drive')
 
 
-def add_ad_to_car_dict(ad):
+def collect_data(ad):
     car_url = ad
     headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0'
+        'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
     }
 
     cookies = {'autoru_gdpr': '1'}
@@ -25,29 +25,25 @@ def add_ad_to_car_dict(ad):
     response.encoding = 'utf-8'
     page = BeautifulSoup(response.text, 'lxml')
     try:
-        json_data = json.loads(
-            page.find('script', type="application/ld+json").string)
+        json_data = json.loads(page.find('script', type="application/ld+json").string)
     except:
         return None
 
     try:
-        catalog_url = page.find(
-            'a', class_='Link SpoilerLink CardCatalogLink SpoilerLink_type_default').get('href')
-        response_catalog = requests.get(catalog_url, headers=headers, cookies=cookies)
+        catalog_url = page.find('a', class_='Link SpoilerLink CardCatalogLink SpoilerLink_type_default').get('href')
+        response_catalog = requests.get(catalog_url, headers=headers)
         response_catalog.encoding = 'utf-8'
         page_catalog = BeautifulSoup(response_catalog.text, 'lxml')  # html.parser
     except:
         page_catalog = None
-        
+
     try:
-        json_data_catalog = json.loads(page_catalog.find(
-            'script', type="application/json", id='initial-state').string)
+        json_data_catalog = json.loads(page_catalog.find('script', type="application/json", id='initial-state').string)
     except:
         json_data_catalog = {}
 
     try:
-        json_data_equip = json.loads(
-            page.find('script', type="application/json", id='initial-state').string)
+        json_data_equip = json.loads(page.find('script', type="application/json", id='initial-state').string)
     except:
         json_data_equip = None
 
@@ -59,221 +55,203 @@ def add_ad_to_car_dict(ad):
 
     try:
         options_dict = json_data_catalog['state']['compare']['selected'][0]['options']
-        options_list = [key for key in list(
-            options_dict.keys()) if options_dict[key] == 1]
+        options_list = [key for key in list(options_dict.keys()) if options_dict[key] == 1]
     except:
         options_list = []
 
-    # 'vendor' feature will be added during EDA
-    # 'model_info' feature is about transcribing models into russian, pass
-    car_dict.update({ad: {}})
+    try: bodyType = json_data['bodyType']
+    except: bodyType = None
 
-    try:
-        car_dict[ad]['car_url'] = json_data['offers']['url']
-    except:
-        car_dict[ad]['car_url'] = None
+    try: brand = json_data['brand']
+    except: brand = None
 
-    try:
-        car_dict[ad]['bodyType'] = json_data['bodyType']
-    except:
-        car_dict[ad]['bodyType'] = None
+    try: color = json_data['color']
+    except: color = None
 
-    try:
-        car_dict[ad]['brand'] = json_data['brand']
-    except:
-        car_dict[ad]['brand'] = None
+    try: complectation_dict = options_list
+    except: complectation_dict = None
 
-    try:
-        car_dict[ad]['color'] = json_data['color']
-    except:
-        car_dict[ad]['color'] = None
+    try: description = json_data['description']
+    except: description = None
 
-    try:
-        car_dict[ad]['complectation_dict'] = options_list
-    except:
-        car_dict[ad]['complectation_dict'] = None
+    try: engineDisplacement = json_data['vehicleEngine']['engineDisplacement']
+    except: engineDisplacement = None
 
-    try:
-        car_dict[ad]['description'] = json_data['description']
-    except:
-        car_dict[ad]['description'] = None
+    try: enginePower = json_data['vehicleEngine']['enginePower']
+    except: enginePower = None
 
-    try:
-        car_dict[ad]['engineDisplacement'] = json_data['vehicleEngine']['engineDisplacement']
-    except:
-        car_dict[ad]['engineDisplacement'] = None
+    try: equipment_dict = json_data_equip['card']['vehicle_info']['equipment']
+    except: equipment_dict = None
 
-    try:
-        car_dict[ad]['enginePower'] = json_data['vehicleEngine']['enginePower']
-    except:
-        car_dict[ad]['enginePower'] = None
+    try: fuelType = json_data['fuelType']
+    except: fuelType = None
 
-    try:
-        car_dict[ad]['equipment_dict'] = json_data_equip['card']['vehicle_info']['equipment']
-    except:
-        car_dict[ad]['equipment_dict'] = None
+    try: image = json_data['image']
+    except: image = None
 
-    try:
-        car_dict[ad]['fuelType'] = json_data['fuelType']
-    except:
-        car_dict[ad]['fuelType'] = None
+    try: mileage = page.find('li', class_='CardInfoRow CardInfoRow_kmAge').find_all('span')[-1].text.replace(u'\xa0', u' ')
+    except: mileage = None
 
-    try:
-        car_dict[ad]['image'] = json_data['image']
-    except:
-        car_dict[ad]['image'] = None
+    try: modelDate = json_data['modelDate']
+    except: modelDate = None
+        
+    model_info = None
 
-    try:
-        car_dict[ad]['mileage'] = page.find('li', class_='CardInfoRow CardInfoRow_kmAge').find_all(
-            'span')[-1].text.replace(u'\xa0', u' ')
-    except:
-        car_dict[ad]['mileage'] = None
+    try: model_name = page.find_all('div', class_='InfoPopup InfoPopup_theme_plain InfoPopup_withChildren BreadcrumbsPopup')[1].text
+    except: model_name = None
 
-    try:
-        car_dict[ad]['modelDate'] = json_data['modelDate']
-    except:
-        car_dict[ad]['modelDate'] = None
+    try: name = json_data['name']
+    except: name = None
 
-    try:
-        car_dict[ad]['model_name'] = page.find_all(
-            'div', class_='InfoPopup InfoPopup_theme_plain InfoPopup_withChildren BreadcrumbsPopup')[1].text
-    except:
-        car_dict[ad]['model_name'] = None
+    try: numberOfDoors = json_data['numberOfDoors']
+    except: numberOfDoors = None
 
-    try:
-        car_dict[ad]['name'] = json_data['name']
-    except:
-        car_dict[ad]['name'] = None
+    parsing_unixtime = int(time.time())
 
-    try:
-        car_dict[ad]['numberOfDoors'] = json_data['numberOfDoors']
-    except:
-        car_dict[ad]['numberOfDoors'] = None
+    try: price = json_data['offers']['price']
+    except: price = None
 
-    try:
-        car_dict[ad]['parsing_unixtime'] = int(time.time())
-    except:
-        car_dict[ad]['parsing_unixtime'] = None
+    try: priceCurrency = json_data['offers']['priceCurrency']
+    except: priceCurrency = None
 
-    try:
-        car_dict[ad]['price'] = json_data['offers']['price']
-    except:
-        car_dict[ad]['price'] = None
+    try: productionDate = json_data['productionDate']
+    except: productionDate = None
 
-    try:
-        car_dict[ad]['priceCurrency'] = json_data['offers']['priceCurrency']
-    except:
-        car_dict[ad]['priceCurrency'] = None
+    try: sell_id = page.find('div', class_='CardHead__infoItem CardHead__id').text[2:]
+    except: sell_id = None
 
-    try:
-        car_dict[ad]['productionDate'] = json_data['productionDate']
-    except:
-        car_dict[ad]['productionDate'] = None
+    try: super_gen = json.loads(page.find('div', id="sale-data-attributes").get('data-bem'))
+    except: super_gen = None
 
-    try:
-        car_dict[ad]['sell_id'] = page.find(
-            'div', class_='CardHead__infoItem CardHead__id').text[2:]
-    except:
-        car_dict[ad]['sell_id'] = None
+    try: vehicleConfiguration = json_data['vehicleConfiguration']
+    except: vehicleConfiguration = None
 
-    try:
-        car_dict[ad]['views'] = page.find(
-            'div', class_='CardHead__infoItem CardHead__views').text.split()[0]
-    except:
-        car_dict[ad]['views'] = None
+    try: vehicleTransmission = json_data['vehicleTransmission']
+    except: vehicleTransmission = None
+        
+    vendor = None
 
-    try:
-        car_dict[ad]['date_added'] = page.find(
-            'div', class_='CardHead__infoItem CardHead__creationDate').text
-    except:
-        car_dict[ad]['date_added'] = None
+    try: owners = page.find('li', class_='CardInfoRow CardInfoRow_ownersCount').find_all('span')[-1].text.replace(u'\xa0', u' ')
+    except: owners = None
 
-    try:
-        car_dict[ad]['super_gen'] = json.loads(
-            page.find('div', id="sale-data-attributes").get('data-bem'))
-    except:
-        car_dict[ad]['super_gen'] = None
+    try: owning = page.find('li', class_='CardInfoRow CardInfoRow_owningTime').find_all('span')[-1].text
+    except: owning = None
 
-    try:
-        car_dict[ad]['vehicleConfiguration'] = json_data['vehicleConfiguration']
-    except:
-        car_dict[ad]['vehicleConfiguration'] = None
+    try: pts = page.find('li', class_='CardInfoRow CardInfoRow_pts').find_all('span')[-1].text
+    except: pts = None
 
-    try:
-        car_dict[ad]['vehicleTransmission'] = json_data['vehicleTransmission']
-    except:
-        car_dict[ad]['vehicleTransmission'] = None
+    try: drive = page.find('li', class_='CardInfoRow CardInfoRow_drive').find_all('span')[-1].text
+    except: drive = None
 
-    try:
-        car_dict[ad]['Владельцы'] = page.find('li', class_='CardInfoRow CardInfoRow_ownersCount').find_all(
-            'span')[-1].text.replace(u'\xa0', u' ')
-    except:
-        car_dict[ad]['Владельцы'] = None
+    try: wheel = page.find('li', class_='CardInfoRow CardInfoRow_wheel').find_all('span')[-1].text
+    except: wheel = None
 
-    try:
-        car_dict[ad]['Владение'] = page.find(
-            'li', class_='CardInfoRow CardInfoRow_owningTime').find_all('span')[-1].text
-    except:
-        car_dict[ad]['Владение'] = None
+    try: status = page.find('li', class_='CardInfoRow CardInfoRow_state').find_all('span')[-1].text
+    except: status = None
 
-    try:
-        car_dict[ad]['ПТС'] = page.find(
-            'li', class_='CardInfoRow CardInfoRow_pts').find_all('span')[-1].text
-    except:
-        car_dict[ad]['ПТС'] = None
+    try: customs = page.find('li', class_='CardInfoRow CardInfoRow_customs').find_all('span')[-1].text
+    except: customs = None
 
-    try:
-        car_dict[ad]['Привод'] = page.find(
-            'li', class_='CardInfoRow CardInfoRow_drive').find_all('span')[-1].text
-    except:
-        car_dict[ad]['Привод'] = None
+    with open("/drive/My Drive/autoru_result_000000.csv", "a") as file:
+        writer = csv.writer(file)
 
-    try:
-        car_dict[ad]['Руль'] = page.find(
-            'li', class_='CardInfoRow CardInfoRow_wheel').find_all('span')[-1].text
-    except:
-        car_dict[ad]['Руль'] = None
-
-    try:
-        car_dict[ad]['Состояние'] = page.find(
-            'li', class_='CardInfoRow CardInfoRow_state').find_all('span')[-1].text
-    except:
-        car_dict[ad]['Состояние'] = None
-
-    try:
-        car_dict[ad]['Таможня'] = page.find(
-            'li', class_='CardInfoRow CardInfoRow_customs').find_all('span')[-1].text
-    except:
-        car_dict[ad]['Таможня'] = None
-
-    try:
-        car_dict[ad]['region'] = page.find('div', class_='CardBreadcrumbs').find_all(
-            'div', class_='CardBreadcrumbs__item')[-1].text.replace(u'\xa0', u' ')
-    except:
-        car_dict[ad]['region'] = None
-
-    with open('/drive/My Drive/autoru_result.csv', 'a') as myfile:
-        wr = csv.writer(myfile)
-        wr.writerow(car_dict[ad].items())
+        writer.writerow(
+            (
+                bodyType,
+                brand,
+                car_url,
+                color,
+                complectation_dict,
+                description, 
+                engineDisplacement, 
+                enginePower, 
+                equipment_dict,
+                fuelType, 
+                image, 
+                mileage, 
+                modelDate, 
+                model_info, 
+                model_name,
+                name, 
+                numberOfDoors, 
+                parsing_unixtime, 
+                price,
+                priceCurrency,
+                productionDate, 
+                sell_id, 
+                super_gen, 
+                vehicleConfiguration,
+                vehicleTransmission, 
+                vendor, 
+                owners, 
+                owning, 
+                pts,
+                drive, 
+                wheel, 
+                status, 
+                customs
+            )
+        )
 
 
 df_ads = pd.read_csv('/drive/My Drive/all_ads.csv', sep=',', header=None)
 all_ads = df_ads[0].tolist()
 
-car_dict = {}
-
 
 def makeDatasetFile(start, end):
     steps = 0
+
+    with open('autoru_result_000000.csv', "w") as file:
+        writer = csv.writer(file)
+
+        writer.writerow(
+            (
+                'bodyType',
+                'brand',
+                'car_url',
+                'color',
+                'complectation_dict',
+                'description', 
+                'engineDisplacement', 
+                'enginePower', 
+                'equipment_dict',
+                'fuelType', 
+                'image', 
+                'mileage', 
+                'modelDate', 
+                'model_info', 
+                'model_name',
+                'name', 
+                'numberOfDoors', 
+                'parsing_unixtime', 
+                'price',
+                'priceCurrency',
+                'productionDate', 
+                'sell_id', 
+                'super_gen', 
+                'vehicleConfiguration',
+                'vehicleTransmission', 
+                'vendor', 
+                'Владельцы', 
+                'Владение', 
+                'ПТС',
+                'Привод', 
+                'Руль', 
+                'Состояние', 
+                'Таможня'
+            )
+        )
+
     for ad in all_ads[start:end]:
-        add_ad_to_car_dict(ad)
+        collect_data(ad)
         steps += 1
         if steps % 20 == 0:
             print(f'Completed {steps} of total {end-start}')
         time.sleep(1)
 
 
-# parse first 150k ads
+# parse top 150k ads
 process = Process(target=makeDatasetFile(0, 150000))
 process.start()
 process.join()
+
