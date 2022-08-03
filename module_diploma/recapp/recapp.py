3426f8ea2ac38740c3abc6b2cd9ea1a7e592e20b
Kirill Kurentsov <immortalsteel@gmail.com>
2022-08-03 19:42:26 +0300

    Flask app
    visitors.csv - https://drive.google.com/file/d/1K3fuYLq4w9liWLo-GyZk5ewYbkHwEZ4X/view?usp=sharing

---
+++ module_diploma/recapp/recapp.py
@@ -0,0 +1,69 @@
+# export FLASK_APP=recapp.py
+# export FLASK_ENV=development
+# flask run
+from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'df0331cefc6c2b9a5d0208a726a5d1c0fd37324feba25506'
+visitors = pd.read_csv('visitors.csv')
+
+
+@app.route('/')
+@app.route('/index/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    start = request.form['action'] == 'Predict'
+    visitorid = int(request.form['visitorid'])
+    if start:
+        # top 3 value_counts() items from train dataset
+        top3 = [5411, 461686, 187946]
+        if visitorid not in visitors.visitorid.values:
+            first, second, third = top3[0], top3[1], top3[2]
+            return '<h1>{}, {}, {}</h1>'.format(first, second, third)
+        else:
+            # Get the row with visitorid
+            row = visitors[visitors.visitorid == visitorid]
+            # Get the list of items from the row
+            items_list = []
+            try:
+                items_list.append(int(row['first'].values))
+            except:
+                items_list.append(np.nan)
+            try:
+                items_list.append(int(row['second'].values))
+            except:
+                items_list.append(np.nan)
+            try:
+                items_list.append(int(row['third'].values))
+            except:
+                items_list.append(np.nan)
+            # Get the number of Nans in the item_list
+            cold_start_count = row.cold_start_count.values[0]
+            # If there is 1 Nan in the row, replace it with top3[0]
+            if cold_start_count == 1:
+                items_list[2] = top3[0]
+                first, second, third = items_list[0], items_list[1], items_list[2]
+                return '<h1>{}, {}, {}</h1>'.format(first, second, third)
+            # If there are 2 Nans in the row, replace it with top3[0] and top3[1]
+            elif cold_start_count == 2:
+                items_list[1] = top3[0]
+                items_list[2] = top3[1]
+                first, second, third = items_list[0], items_list[1], items_list[2]
+                return '<h1>{}, {}, {}</h1>'.format(first, second, third)
+            # If there are 3 Nans in the row, replace it with top3[0], top3[1] and top3[2]
+            elif cold_start_count == 3:
+                items_list[0] = top3[0]
+                items_list[1] = top3[1]
+                items_list[2] = top3[2]
+                first, second, third = items_list[0], items_list[1], items_list[2]
+                return '<h1>{}, {}, {}</h1>'.format(first, second, third)
+            else:
+                first, second, third = items_list[0], items_list[1], items_list[2]
+                return '<h1>{}, {}, {}</h1>'.format(first, second, third)
+        
